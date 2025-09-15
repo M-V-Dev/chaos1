f758a37a47e695157ab6c55b74a10d821ad02900
M_V_Dev <mcvancayseele@gmail.com>
2025-09-15 16:49:06 +0200

    Another update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { usePumpfunTokenTrades } from "./usePumpfunTokenTrades";
 
-const TOKEN_KEY = "HEXYuc5RumPYejirQeL6Nb4cAqLXPVh6qVAsy9JNpump";
+const TOKEN_KEY = "HmmifedbYo6qeQRA9CbhR1mvWcRFKu5YHWvpTuN6pump";
 
 // 3 random images for buys & sells
 const buyImages = ["/images/buy1.png", "/images/buy2.png", "/images/buy3.png"];
